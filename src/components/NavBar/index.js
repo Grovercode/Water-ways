45613f9fda629e4f7f8101d844bc8ae7566229f4
Hriday Grover <groverhriday12@gmail.com>
2023-10-08 12:20:45 +0300

    Browser support v1

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -5,7 +5,7 @@ import Logo from "../../assets/fish.png";
 const Wrapper = styled.div`
   width: 100%;
   padding: 10px 0px;
-  z-index: 1;
+  z-index: 2;
   position: fixed;
   align-items: center;
   box-shadow: 2px 4px 12px 0px rgba(0, 0, 0, 0.25);
@@ -17,7 +17,12 @@ const Wrapper = styled.div`
     0.2
   ); /* Set a transparent background color */
   &:before {
+    content: "";
+    z-index: -1;
+    top: 0;
+    left: 0;
     width: 100%;
+    height: 100%;
     backdrop-filter: blur(8px);
     -webkit-backdrop-filter: blur(10px);
   }
